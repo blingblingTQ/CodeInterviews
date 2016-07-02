5bfb8435c1065cea6f19605b7b75e3abb1725bd6
tangqi <tangqi@iie.ac.cn>
2016-07-02 14:54:09 +0800

    二维数组中的查找，添加两种解法思路

--- 二维数组中的查找.cpp
+++ 二维数组中的查找.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+/* 解法一：
+ * M行N列的矩阵，对每一行进行二分查找，平均时间复杂度是O(Mlog(N))
+ **/
+
+/* 解法二：
+ * 采用二分查找法，每个子矩阵中，左上角最小，右下角最大。
+ * 按照对角线的顺序进行二分搜索
+ * 参考《程序员面试金典》P260
+ */
+
+/* 解法三：
+ * 从右上角开始搜索
+ */
 bool find(int *matrix, int rows, int columns, int number)
 {
     if(matrix == NULL || rows <= 0 || columns <= 0)
