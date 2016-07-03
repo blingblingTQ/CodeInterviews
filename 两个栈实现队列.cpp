723da3714f35dcfb29db97c8ec38281b549ec6e6
tangqi <tangqi@iie.ac.cn>
2016-07-03 20:36:59 +0800

    两个栈实现队列

---
+++ 两个栈实现队列.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Solution{
+private:
+    stack<int> s1, s2;
+public:
+    /* 入队列，总是向第一个栈入 */
+    void enqueue(int x)
+    {
+        s1.push(x);
+    }
+
+    /* 出队列，如果s2是空，就把s1的元素全部灌入到s2中，最后总是从s2中出队列 */
+    int dequeue(int x)
+    {
+        if(s2.empty())
+        {
+            while(s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int val = s2.top();
+        s2.pop();
+        return val;
+    }
+};
