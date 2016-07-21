6669d175dda8012dca173e9ceff19a4e652fb60b
tangqi <tangqi@iie.ac.cn>
2016-07-21 14:27:47 +0800

    数组中重复的数字

---
+++ 数组中重复的数字.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int duplicate_number(vector<int>& nums)
+{
+    int size = nums.size();
+    if(size == 0)
+        return -1;
+    int duplicate = -1;
+    for(int i = 0; i < size; i++)
+    {
+        while(nums[i] != i)
+        {
+            if(nums[i] == nums[nums[i]])
+            {
+                duplicate = nums[i];
+            }
+
+            int tmp = nums[i];
+            nums[i] = nums[tmp];
+            nums[tmp] = tmp;
+        }
+    }
+}
