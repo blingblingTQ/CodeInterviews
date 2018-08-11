45162752c47c31f35205efb7c7b6248a68d4312c
tangqi <tangqi@iie.ac.cn>
2018-08-11 21:12:28 +0800

    fixbug

--- 两个栈实现队列.cpp
+++ 两个栈实现队列.cpp
@@ -17,7 +17,7 @@ public:
     {
         if(s2.empty())
         {
-            while(s1.empty())
+            while(!s1.empty())
             {
                 s2.push(s1.top());
                 s1.pop();
